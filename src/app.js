24c0f7aaef3d28cbe7f9fb4ce8b29003b4952956
Paola Mangelli <mangelli.paola90@gmail.com>
2023-05-01 15:18:37 -0400

    cartas al azar 1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cards = ["♦", "♥", "♠", "♣"];
+
+  function displayCard() {
+    let oneCard = cards[Math.floor(Math.random() * cards.length)];
+    let oneNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    let showCard = `${oneCard}`;
+    let showNumber = `${oneNumber}`;
+    document.getElementsByClassName("pinta").innerHTML = showCard;
+    document.getElementsByClassName("valor").innerHTML = showNumber;
+  }
 };
