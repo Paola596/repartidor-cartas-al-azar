6f07144c07c823a709f359aedd2857bc7cfa20ae
Paola Mangelli <mangelli.paola90@gmail.com>
2023-05-02 11:08:24 -0400

    last push

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   const numbers = [
     "A",
@@ -21,7 +19,6 @@ window.onload = function() {
     "K"
   ];
   const cards = ["♦", "♥", "♠", "♣"];
-
   function displayCard() {
     let oneCard = cards[Math.floor(Math.random() * cards.length)];
     let oneNumber = numbers[Math.floor(Math.random() * numbers.length)];
@@ -29,7 +26,13 @@ window.onload = function() {
     let showNumber = `${oneNumber}`;
     document.querySelector(".pinta").innerHTML = showCard;
     document.querySelector(".valor").innerHTML = showNumber;
+    document.querySelector("#invertPinta").innerHTML = showCard;
   }
+  const btbn = document.querySelector("#btn");
+  btbn.addEventListener("click", () => {
+    location.reload();
+  });
+  displayCard();
 };
 
 console.log("Hello Rigo from the console!");
